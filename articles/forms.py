9820f42458fdaf6bcca8c6ad6c1e8ed8b6ab74fe
amebrahimi <amirebrahimi5@live.com>
2022-09-18 09:42:12 +0000

    register with forms

--- articles/forms.py
+++ articles/forms.py
@@ -1,10 +1,28 @@
 from django import forms
 
-class ArticleForm(forms.Form):
-	title = forms.CharField()
-	content = forms.CharField()
+from articles.models import Article
 
-	def clean_title(self):
-		cleaned_data = self.cleaned_data
-		title = cleaned_data.get('title')
-		return title
+
+class ArticleForm(forms.ModelForm):
+
+    class Meta:
+        model = Article
+        fields = ['title', 'content']
+
+    def clean(self):
+        data = self.cleaned_data
+        title = data.get('title')
+        qs = Article.objects.filter(title__icontains=title)
+        if (qs.exists()):
+            self.add_error('title', f'"{title}" is already in use.')
+        return data
+
+
+class ArticleFormOld(forms.Form):
+    title = forms.CharField()
+    content = forms.CharField()
+
+    def clean_title(self):
+        cleaned_data = self.cleaned_data
+        title = cleaned_data.get('title')
+        return title
--- articles/views.py
+++ articles/views.py
@@ -1,44 +1,46 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .forms import ArticleForm
-from .models import Article
+from articles.forms import ArticleForm
+from articles.models import Article
 
 # Create your views here.
 
+
 def article_search_view(request):
-	query_dict = request.GET
+    query_dict = request.GET
 
-	try:
-		query = int(query_dict.get('q'))
-	except:
-		query = None
+    try:
+        query = int(query_dict.get('q'))
+    except:
+        query = None
 
-	article_obj = None
+    article_obj = None
 
-	if(query is not None):
-		article_obj = Article.objects.get(id=query)
+    if (query is not None):
+        article_obj = Article.objects.get(id=query)
 
-	context= {
-		"object": article_obj
-	}
-	return render(request, "articles/search.html", context=context)
+    context = {
+        "object": article_obj
+    }
+    return render(request, "articles/search.html", context=context)
+
 
 @login_required
 def article_create_view(request):
-	form = ArticleForm(request.POST or None)
-	context = {
-		'form': ArticleForm()
-	}
+    form = ArticleForm(request.POST or None)
+    context = {
+        'form': form
+    }
 
-	if form.is_valid():
-		title = form.cleaned_data.get('title')
-		content = form.cleaned_data.get('content')
-		article_object = Article.objects.create(title=title, content=content)
-		context['object'] = article_object
-		context['created'] = True
+    if form.is_valid():
+        article_object = form.save()
+        context['form'] = ArticleForm()
+        # title = form.cleaned_data.get('title')
+        # content = form.cleaned_data.get('content')
+        # article_object = Article.objects.create(title=title, content=content)
 
-	return render(request, "articles/create.html", context=context)
+    return render(request, "articles/create.html", context=context)
 # def article_create_view(request):
 # 	context = {
 # 		'form': ArticleForm()
@@ -55,13 +57,11 @@ def article_create_view(request):
 # 	return render(request, "articles/create.html", context=context)
 
 
-
 def article_detail_view(request, id=None):
-	article_obj = None
-	if id is not None:
-		article_obj = Article.objects.get(id=id)
-	context = {
-		"object": article_obj
-	}
-	return render(request, "articles/detail.html", context=context)
-
+    article_obj = None
+    if id is not None:
+        article_obj = Article.objects.get(id=id)
+    context = {
+        "object": article_obj
+    }
+    return render(request, "articles/detail.html", context=context)
